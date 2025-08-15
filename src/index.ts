8f56f77893f2e1830031471fa16428d434fef5fc
Fiflon <odzera.dotrofera@gmail.com>
2025-08-15 20:09:03 +0200

    Add createPythonProject tool

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,54 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import z from "zod";
-
+import { createPythonProjectSchema } from "./templates/create_python_project/createPythonProjectSchema.js";
+import { newProjectTemplate } from "./templates/create_python_project/createPythonProjectTemplate.js";
+import createPythonFile from "./utils/fileManipulation.js";
 
 const server = new McpServer({
-  name: "blender-editor-mcp",
-  version: "1.0.0",
-  capabilities: {
-    resources: {},
-    tools: {},
-  },
+    name: "blender-editor-mcp",
+    version: "1.0.0",
+    capabilities: {
+        resources: {},
+        tools: {},
+    },
 });
 
 server.tool(
-    "get_hello",
-    "Get greeting from a server to a specific user",
-    {
-        name: z.string().min(2).describe("Name of the person to greet"),
-    },
-    async ({ name }) => {
-        return {
-            content: [
-                {
-                    type: "text",
-                    text: `Hello, ${name}! Welcome to the Blender MCP Server.`,
-                },
-            ],
-        };
+    "create_python_project",
+    "Create Python script for video edit that allows for futher modifications with other tools",
+
+    createPythonProjectSchema.shape,
+
+    async ({ videoOutputPath, pythonScriptOutputPath, fps }) => {
+        try {
+            const pythonCode = newProjectTemplate({ fps, videoOutputPath });
+            const finalPath = createPythonFile(
+                pythonScriptOutputPath,
+                pythonCode
+            );
+
+            return {
+                content: [
+                    {
+                        type: "text",
+                        text: `Python script created successfully!\n\n**File:** ${finalPath}\n\n**Content:**\n\`\`\`python\n${pythonCode}\`\`\``,
+                    },
+                ],
+            };
+        } catch (error) {
+            return {
+                content: [
+                    {
+                        type: "text",
+                        text: `Error creating Python script: ${
+                            error instanceof Error
+                                ? error.message
+                                : "Unknown error"
+                        }`,
+                    },
+                ],
+            };
+        }
     }
 );
 
